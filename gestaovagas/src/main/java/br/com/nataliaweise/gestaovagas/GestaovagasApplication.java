7ce202704bf9cfb757fb6e60ad00903de184ff9d
Natália Sens Weise <nataliaweise@gmail.com>
2024-12-06 11:25:41 -0300

    feat: creating repository

--- gestaovagas/src/main/java/br/com/nataliaweise/gestaovagas/GestaovagasApplication.java
+++ gestaovagas/src/main/java/br/com/nataliaweise/gestaovagas/GestaovagasApplication.java
@@ -3,7 +3,7 @@ package br.com.nataliaweise.gestaovagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //define q é aqui o main do projeto
 public class GestaovagasApplication {
 
 	public static void main(String[] args) {
--- gestaovagas/src/main/java/br/com/nataliaweise/gestaovagas/modules/candidate/controllers/CandidateController.java
+++ gestaovagas/src/main/java/br/com/nataliaweise/gestaovagas/modules/candidate/controllers/CandidateController.java
@@ -1,17 +1,20 @@
 package br.com.nataliaweise.gestaovagas.modules.candidate.controllers;
 import br.com.nataliaweise.gestaovagas.modules.candidate.CandidateEntity;
+import br.com.nataliaweise.gestaovagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/candidate")
+@RestController //define que é um arquivo de rotas
+@RequestMapping("/candidate") //define caminho inicial
 public class CandidateController {
 
-    @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidate){
-        
+    @Autowired //td q estiver abaixo o spring vai instanciar
+    private CandidateRepository candidateRepository;
+
+    @PostMapping("/") //define metodo e rota
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidate){
+        return this.candidateRepository.save(candidate);
     }
 }
